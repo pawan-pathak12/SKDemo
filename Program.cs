2a4c154c99f2cd88fb3862dc9744bf09823dd000
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2025-10-06 11:45:43 +0545

    Code work fine : code before adding plugin

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var builder = Kernel.CreateBuilder();
+
+// Services 
+
+#region Connecting AI Serivce
+
+var groqApiKey = Environment.GetEnvironmentVariable("GROQ_API_KEY");
+
+builder.AddOpenAIChatCompletion(
+    "llama-3.1-8b-instant",
+    apiKey: groqApiKey,
+    endpoint: new Uri("https://api.groq.com/openai/v1")
+);
+
+#endregion
+
+//PLugins 
+var kernel = builder.Build();
+
+var chatService = kernel.GetRequiredService<IChatCompletionService>();
+var chatMessages = new ChatHistory();
+
+while (true)
+{
+    Console.WriteLine("Prompt:");
+    chatMessages.AddUserMessage(Console.ReadLine()!);
+
+    var completion = chatService.GetStreamingChatMessageContentsAsync(chatMessages, kernel: kernel);
+
+    var fullMessage = "";
+
+    await foreach (var context in completion)
+    {
+        Console.Write(context.Content);
+        fullMessage = context.Content;
+    }
+
+    chatMessages.AddAssistantMessage(fullMessage);
+    Console.WriteLine();
+}
