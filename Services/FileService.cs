b26a6bc831a725479db3d51ad9dcc11a9a856b32
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2025-10-14 21:21:10 +0545

    Started adding fileService to save conversation

---
+++ Services/FileService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace SKDemo.Services;
+
+public class FileService
+{
+    private static readonly string HistoryFile = "chat_history.json";
+
+    public static void SaveConversation(ChatHistory chatHistory)
+    {
+    }
+}
